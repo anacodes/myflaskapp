1d2363e4efadbf485adf1012f17b27381738c2b5
anacodes <anaghaajnadkar@gmail.com>
2019-09-30 17:59:20 +0530

    create a simple website

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article one',
+            'body': 'ASDFGHKNGSJSNKS',
+            'author': 'Anagha',
+            'date': '25-04-2019',
+        },
+                {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'ASDFGHKNGSJSNKS',
+            'author': 'Anagha',
+            'date': '25-04-2019',
+        },
+                {
+            'id': 3,
+            'title': 'Article three',
+            'body': 'ASDFGHKNGSJSNKS',
+            'author': 'Anagha',
+            'date': '25-04-2019',
+        }
+    ]
+    return articles
